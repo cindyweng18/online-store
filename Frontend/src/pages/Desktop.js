f8189bdad778eeebb98431488e523b85626e7348
cindyweng18 <cindyweng18@gmail.com>
2021-04-23 18:56:20 -0400

    created footer and made small changes to files

---
+++ Frontend/src/pages/Desktop.js
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+
+const Desktop = () => {
+  return (
+    <>
+    <Nav />
+    <div>
+      hellooooooo
+    </div>
+    </>
+  );
+}
+
+export default Desktop;
---
+++ Frontend/src/pages/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer class="footer mt-auto py-3 bg-light">
+            <div class="container">
+                <span class="text-muted"> &copy; Copyright {new Date().getFullYear()} Team A. </span>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
