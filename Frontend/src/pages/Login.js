60b78cecf331d353e48a554bb46ce54c92a79b3c
cindyweng18 <cindyweng18@gmail.com>
2021-04-28 12:03:52 -0400

    updated login page and account page

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,29 +1,52 @@
 import Footer from "./Footer";
 import Nav from "./Nav";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import axios from "axios";
 
 function Login() {
+    const history = useHistory();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const [message, setMessage] = useState("");
   
     function validateForm() {
       return email.length > 0 && password.length > 0;
     };
   
-    function handleSubmit(event) {
+    function handleSubmit(event, route) {
       event.preventDefault();
+      axios
+      .post("/login", {
+        email: email,
+        password: password,
+      })
+      .then(async (response) => {
+        localStorage.clear()
+        localStorage.setItem("route", route);
+        localStorage.setItem("session", response.data['loginData']['fullName'].replace(" ", ""));
+        history.push(`/${route}/${response.data['loginData']['fullName'].replace(" ", "")}`);
+      })
+      .catch((e) => setMessage("error"));
+      };
+
+      // useEffect(() => {
+      //   const route = localStorage.getItem("route");
+      //   const session = localStorage.getItem("session");
+      //   if (localStorage.getItem("session") !== null)
+      //   history.push(`/${route}/${session}`);
+      // }, [history]);
       
-    };
 
     return (
         <>
         <Nav />
         <div className="Login">
             <h1> Sign In </h1>
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={(e) => handleSubmit(e, "account")}>
             <Form.Group size="lg" controlId="email">
               <Form.Label>Email</Form.Label>
               <Form.Control
--- Frontend/src/pages/Routes.js
+++ Frontend/src/pages/Routes.js
@@ -7,18 +7,23 @@ import Laptop from "./Laptop";
 import Parts from "./Parts";
 import Login from "./Login";
 import Register from "./Register";
+import { Context } from './ContextApi';
+import Account from "./Account";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+    <Context>
       <Switch>
         <Route exact={true} path={"/"} component={Home} />
         <Route exact={true} path={"/login"} component={Login} />
         <Route exact={true} path={"/register"} component={Register} />
+        <Route exact={true} path={"/account/:user"} component={Account} />
         <Route exact={true} path={"/desktops"} component={Desktop} />
         <Route exact={true} path={"/laptops"} component={Laptop} />
         <Route exact={true} path={"/desktops/:os/:purpose/:arch"} component={Parts} />
       </Switch>
+      </Context>
     </BrowserRouter>
   );
 };
